4985b9871ff17a6be1530bce24e785db5bebca59
fedeghe <fedeghe@gmail.com>
2020-09-30 00:47:57 +0200

    try root magicMethod

--- src/sample/indexTable.js
+++ src/sample/indexTable.js
@@ -2,7 +2,11 @@
     var target = document.getElementById('target'),
         config = {
             target: target,
-            
+            method_rndColor: function () {
+                return '#' + [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'].sort(function (a,b) {
+                    return Math.random() > 0.5 ? 1 : -1;
+                }).slice(0,6).join('');
+            },
             children: [{
                 tag: 'table',
                 children: function () {
@@ -16,17 +20,13 @@
                                     return {
                                         tag: 'td',
                                         html: '[' + i + ', '+ j + ']',
-                                        method_rndColor: function () {
-                                            return '#' + [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'].sort(function (a,b) {
-                                                return Math.random() > 0.5 ? 1 : -1;
-                                            }).slice(0,6).join('');
-                                        },
+                                        
                                         cb: function () {
                                             var self = this;
                                             clearTimeout(self.data.to);
                                             self.data.to = setTimeout(function () {
                                                 self.config.html = ~~(10*Math.random());
-                                                self.node.style.backgroundColor = self.rndColor();
+                                                self.node.style.backgroundColor = self.rootNode.rndColor();
                                                 self.render();
                                             }, ~~(Math.random()*5000));
                                             self.done();
