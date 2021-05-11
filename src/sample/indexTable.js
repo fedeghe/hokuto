72c274ef3065d576fc91db0126d086c2ebcd9aa9
fedeghe <fedeghe@gmail.com>
2021-05-11 22:49:07 +0200

    update wpm

--- src/sample/indexTable.js
+++ src/sample/indexTable.js
@@ -1,64 +1,64 @@
-(function () {
+(function() {
     var target = document.getElementById('target'),
         config = {
             target: target,
-            method_rndColor: function () {
-                return '#' + [0,1,2,3,4,5,6,7].sort(function (a,b) {
+            method_rndColor: function() {
+                return '#' + [0, 1, 2, 3, 4, 5, 6, 7].sort(function(a, b) {
                     return Math.random() > 0.5 ? 1 : -1;
-                }).slice(0,6).join('');
+                }).slice(0, 6).join('');
             },
-            method_rndRGB: function () {
+            method_rndRGB: function() {
                 return [
                     'red', 'green', 'blue'
                 ][Math.floor(Math.random() * 3)]
             },
-            method_rnd: function () {
-                return 1e3*Math.random().toFixed(3)
+            method_rnd: function() {
+                return 1e3 * Math.random().toFixed(3)
             },
-            
             children: [{
                 tag: 'table',
                 ref: 'tab1',
                 data: {
                     rows: 10, // 100,
-                    cols: 20  // 7
+                    cols: 20 // 7
                 },
-                children: function () {
+                children: function() {
                     // console.log('table ctx', this);
-                    
-                    return new Array(this.data.rows).fill().map(function (_, i) {
+                    return new Array(this.data.rows).fill().map(function(_, i) {
                         return {
                             tag: 'tr',
-                            children: function () {
+                            children: function() {
                                 // console.log('tr ctx', this);
                                 var self = this
-                                return new Array(this.parentNode.data.cols).fill().map(function (_, j) {
+                                return new Array(this.parentNode.data.cols).fill().map(function(_, j) {
                                     return {
                                         tag: 'td',
-                                        style:{
-                                            minWidth:'20px',
-                                            width:'60px',
-                                            height:'20px',
+                                        style: {
+                                            minWidth: '20px',
+                                            width: '60px',
+                                            height: '20px',
                                             textAlign: 'center',
-                                            fontSize:'18px',
+                                            fontSize: '18px',
                                             userSelect: 'none',
                                             cursor: 'pointer'
                                         },
-                                        state: function () {return {
-                                            tab1: self.getNode('tab1')
-                                        }},
+                                        state: function() {
+                                            return {
+                                                tab1: self.getNode('tab1')
+                                            }
+                                        },
                                         html: self.rootNode.rnd(),
-                                        onMouseover: function () {
+                                        onMouseover: function() {
                                             this.setStyle({
                                                 backgroundColor: 'gray',
                                             });
                                         },
-                                        onMouseout: function () {
+                                        onMouseout: function() {
                                             this.setStyle({
                                                 backgroundColor: 'black',
                                             });
                                         },
-                                        onClick: function (e) {
+                                        onClick: function(e) {
                                             var table = this.state.tab1,
                                                 rootNode = this.rootNode;
                                             this.setHtml(rootNode.rnd());
@@ -71,10 +71,10 @@
                                             });
                                             this.killEvent(e);
                                         },
-                                        cb: function () {
+                                        cb: function() {
                                             var self = this;
 
-                                            // var id = requestAnimationFrame(function () {
+                                            // var id = requestAnimationFrame(function() {
                                             //     self.setHtml(self.rootNode.rnd());
                                             //     self.setStyle({
                                             //         backgroundColor: self.rootNode.rndColor(),
@@ -87,15 +87,15 @@
                                             self.done();
                                         }
                                     }
-                                })          
+                                })
                             }
                         }
                     })
                 }
             }]
         };
-    hokuto.render(config).then(function (t) {
+    hokuto.render(config, true).then(function(t) {
         console.log(t)
     })
-    
+
 })();
