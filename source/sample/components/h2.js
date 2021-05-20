ea5a85dea107a5aa20eb6ea8a6a8ce85674eeae4
fedeghe <fedeghe@gmail.com>
2021-05-20 23:26:27 +0200

    rnd color

--- source/sample/components/h2.js
+++ source/sample/components/h2.js
@@ -5,8 +5,18 @@ var t = {
     state: {
         times: 0
     },
+    style: {
+        cursor: 'pointer'
+    },
+    method_rndColor: function() {
+        return '#' + [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'].sort(function(a, b) {
+            return Math.random() > 0.5 ? 1 : -1;
+        }).slice(0, 6).join('');
+    },
     onClick: function() {
         this.state.times++;
         this.node.innerHTML = this.state.times
+        this.node.style.color = this.rndColor();
+        this.node.blur()
     }
 };
